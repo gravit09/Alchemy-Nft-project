7431d2b167d1e0d029c4729d4fd9032bf34a687e
Gravit09 <garvitchoudhary09@gmail.com>
2024-05-01 08:53:56 +0530

    Nft marketplace using alchemy

---
+++ hardhat.config.js
@@ -0,0 +1,36 @@
+require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-ethers");
+const fs = require("fs");
+// const infuraId = fs.readFileSync(".infuraid").toString().trim() || "";
+
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
+module.exports = {
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {
+      chainId: 1337,
+    },
+    sepolia: {
+      url: "https://eth-sepolia.g.alchemy.com/v2/acKIh3VP0JVALbM9ZWus3ibteeuVU9Ul",
+      accounts: [
+        "8ca9c3454d2ba45a80fe91b3a6be806201c3f3e5e49986eb6406723867a83ce5",
+      ],
+    },
+  },
+  solidity: {
+    version: "0.8.4",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+};
